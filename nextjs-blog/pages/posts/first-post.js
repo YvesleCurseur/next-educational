4e1bfafd8dca5e0c8b07643943cb4842ff906b4f
YvesleCurseur <pognonyvesfulbert@gmail.com>
2023-03-04 23:19:18 +0000

    Start the blog app in next

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+    return (
+        <>
+        
+            <h1>First Post</h1>
+
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+
+        </>
+    
+    );
+}
